d7ad713ca1266055e0cc63f33663563b1b52e876
navpreet.ahuja@hotmail.com <navpreet.ahuja@hotmail.com>
2018-04-07 17:20:32 +0530

    added grid on submit

--- designs.js
+++ designs.js
@@ -1,10 +1,15 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
-}
+// when size is submitted grid is formed
+$('#sizePicker').submit(function(event){
+	event.preventDefault();
+	let rows=$('#inputHeight').val();
+	let cols=$('#inputWeight').val();
+	let canvas = $('#pixelCanvas');
+	canvas.empty();
+	for(let i=0; i<rows; i++){
+		canvas.append('<tr></tr>');
+		let lastRow = $('#pixelCanvas tr:last-child');
+		for(let j=0; j<cols; j++){
+			lastRow.append('<td></td>');
+		}
+	}
+});
